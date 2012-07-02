9907f17b1a063faa91f6b130acf39e9736c046f5
Atul Varma <varmaa@gmail.com>
2012-07-02 05:25:53 -0400

    Split tests into separate files.

---
+++ static/test/test-linkify.js
@@ -0,0 +1,43 @@
+describe('linkifyTextToHTML', function() {
+  it('should wrap solitary URLs in anchor tags', function() {
+    expect(linkifyTextToHTML('http://foo.com'))
+      .to.be('<a href="http://foo.com">http://foo.com</a>');
+  });
+
+  it('should wrap URLs in beginning of text in anchor tags', function() {
+    expect(linkifyTextToHTML('hi http://foo.com'))
+      .to.be('hi <a href="http://foo.com">http://foo.com</a>');
+  });
+
+  it('should wrap URLs in end of text in anchor tags', function() {
+    expect(linkifyTextToHTML('http://foo.com, dawg.'))
+      .to.be('<a href="http://foo.com">http://foo.com</a>, dawg.');
+  });
+
+  it('should wrap URLs in middle of text in anchor tags', function() {
+    expect(linkifyTextToHTML('it is http://foo.com.'))
+      .to.be('it is <a href="http://foo.com">http://foo.com</a>.');
+  });
+
+  it('should wrap multiple URLs in anchor tags', function() {
+    expect(linkifyTextToHTML('they are http://foo.com and http://bar.com.'))
+      .to.be('they are <a href="http://foo.com">http://foo.com</a> and ' +
+             '<a href="http://bar.com">http://bar.com</a>.');
+  });
+
+  it('should wrap https URLs in anchor tags', function() {
+    expect(linkifyTextToHTML('https://foo.com'))
+      .to.be('<a href="https://foo.com">https://foo.com</a>');
+  });
+
+  it('should not wrap javascript: URLs', function() {
+    expect(linkifyTextToHTML('javascript:alert("NO U")'))
+      .to.be('javascript:alert("NO U")');
+  });
+
+  it('should accept a callback that modifies the anchor', function() {
+    expect(linkifyTextToHTML('http://foo.com', function(a) {
+      a.setAttribute("href", "http://different.com");
+    })).to.be('<a href="http://different.com">http://foo.com</a>');
+  });
+});
---
+++ static/test/test-pretty-date.js
@@ -0,0 +1,46 @@
+describe('prettyDate()', function() {
+  it('should return "in the future" when appropriate', function() {
+    var now = Date.now();
+    expect(prettyDate(now+1, now)).to.be("in the future");
+  });
+
+  it('should return "just now" when appropriate', function() {
+    var now = Date.now();
+    expect(prettyDate(now, now+1)).to.be("just now");
+  });
+
+  it('should return "1 minute ago" when appropriate', function() {
+    var now = Date.now();
+    expect(prettyDate(now, now + 1000*60)).to.be("1 minute ago");
+  });
+
+  it('should return "2 minutes ago" when appropriate', function() {
+    var now = Date.now();
+    expect(prettyDate(now, now + 1000*60*2)).to.be("2 minutes ago");
+  });
+
+  it('should return "1 hour ago" when appropriate', function() {
+    var now = Date.now();
+    expect(prettyDate(now, now + 1000*60*60)).to.be("1 hour ago");
+  });
+
+  it('should return "2 hours ago" when appropriate', function() {
+    var now = Date.now();
+    expect(prettyDate(now, now + 1000*60*60*2)).to.be("2 hours ago");
+  });
+
+  it('should return "Yesterday" when appropriate', function() {
+    var now = Date.now();
+    expect(prettyDate(now, now + 1000*60*60*24)).to.be("Yesterday");
+  });
+
+  it('should return "7 days ago" when appropriate', function() {
+    var now = Date.now();
+    expect(prettyDate(now, now + 1000*60*60*24*7)).to.be("7 days ago");
+  });
+
+  it('should return "2 weeks ago" when appropriate', function() {
+    var now = Date.now();
+    expect(prettyDate(now, now + 1000*60*60*24*14)).to.be("2 weeks ago");
+  });
+});

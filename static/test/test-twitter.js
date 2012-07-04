386d8c7da209fe1ca9a9f5eea99f9cc469389f8c
Atul Varma <varmaa@gmail.com>
2012-07-04 11:52:08 -0400

    added TwitterViewMixIn tests.

--- static/test/test-twitter.js
+++ static/test/test-twitter.js
@@ -1,7 +1,23 @@
-defineTests(["event-emitter", "twitter"], function(EventEmitter, twitter) {
+defineTests([
+  "jquery",
+  "event-emitter",
+  "twitter",
+  "user-list-view",
+  "users"
+], function($, EventEmitter, twitter, UserListView, Users) {
+  function FakeIRC() { this.mappings = {}; }
+  
+  FakeIRC.prototype = {
+    setCustomGlobalMetadata: function(key, value) {
+      this.mappings[key] = value;
+    }
+  };
+  
+  EventEmitter.mixInto(FakeIRC);
+  
   describe("TwitterUsers", function() {
     it("should update from IRC load of custom global data", function() {
-      var irc = EventEmitter.create();
+      var irc = new FakeIRC();
       var tu = new twitter.TwitterUsers(irc);
       irc.emit('custom-global-metadata', {
         'twitter-mapping/atul': 'toolness'
@@ -14,17 +30,38 @@ defineTests(["event-emitter", "twitter"], function(EventEmitter, twitter) {
     });
     
     it("should update itself and the server from set()", function() {
-      var irc = EventEmitter.create();
-      var mappings = {};
-      irc.setCustomGlobalMetadata = function(key, value) {
-        mappings[key] = value;
-      };
+      var irc = new FakeIRC();
       var tu = new twitter.TwitterUsers(irc);
       tu.set("atul", "toolness");
       expect(tu.getAvatarElement('atul|otp')).to.have.length(1);
-      expect(mappings).to.eql({
+      expect(irc.mappings).to.eql({
         'twitter-mapping/atul': 'toolness'
       });
+    });
+  });
+  
+  describe("TwitterViewMixIn", function() {
+    function makeIt() {
+      var irc = new FakeIRC();
+      var tu = new twitter.TwitterUsers(irc);
+      var ulv = new UserListView(new Users(), $('<div></div>'));
+      return new twitter.TwitterViewMixIn(ulv, tu);
+    }
+    
+    it("should update when TwitterUsers instance updates", function() {
+      var t = makeIt();
+      t.userListView.users.add("atul-away", "#a");
+      expect(t.userListView.el.find('.twitter-user')).to.have.length(0);
+      t.twitterUsers.set("atul", "toolness");
+      expect(t.userListView.el.find('.twitter-user')).to.have.length(1);
+    });
+
+    it("should update when Users instance updates", function() {
+      var t = makeIt();
+      t.twitterUsers.set("atul", "toolness");
+      expect(t.userListView.el.find('.twitter-user')).to.have.length(0);
+      t.userListView.users.add("atul-away", "#a");
+      expect(t.userListView.el.find('.twitter-user')).to.have.length(1);
     });
   });
 });
